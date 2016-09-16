28980c282a910cda79106a92be84d93b527ea644
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-16 09:16:03 -0400

    1

--- src/Students.java
+++ src/Students.java
@@ -1,15 +1,35 @@
+	/*This application will do four things. 
+	 	* 1. It will provide info about students in my Java class.
+		* 2. It will prompt the user to ask about a particular student.
+		* 3. It will proper responses according to user submitted info.
+		* 4. It will ask user if they want to learn about another student. */	
+
+	/*This application needs 
+	 	* 1. 
+	 	* 2. 
+	 	* 3. */	
+
 import java.util.Scanner;
 
 public class Students {
 
-// 
-		public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
+		public static void main(String[] args) { //main method
+		System.out.println("Welcome to our Java class.\n"); //welcome message
+		
+		//String array with all students with corresponding number
+		//String array with all the hometown's listed
+		//String array with all the food
+		
+		//which student would you like to learn more about 1-16
+		
+		
+		//array names
+		//array home town
+		//array food
+		
 			
-		System.out.println("Welcome to our Java class. Which student would you like to learn more about?");		
-		String word = sc.nextLine();
-		System.out.println(word);	
-		sc.close();
+		
+	
 	}
 
 } 
